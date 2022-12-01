06cb4dfcd05b525dfae0460a87cf0a771981b468
Lars Poetzel <lars.poetzel@gmail.com>
2022-12-01 15:17:42 +0100

    popup post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,27 @@
 import { useState } from "react";
-
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 const Post = ({ post }) => {
   // const [show, setShow] = useState(false);
   return (
     <article>
-      <h3 id="von-bis">
+      <h2>{post.name}</h2>
+      <h3>Opening times: {post.oeffnungszeiten}</h3>
+      <h4 id="von-bis">
         From: {post.von} to {post.bis}
-      </h3>
-      <br />
-      <p>{post.name}</p>
+      </h4>
+      <p>District: {post.bezirk}</p>
+      <Popup
+        trigger={<button> More Information</button>}
+        position="right center"
+      >
+        <div>
+          <h3>{post.name}</h3>
+          <div>
+            Adress: {post.strasse} , {post.plz_ort}
+          </div>
+        </div>
+      </Popup>
     </article>
   );
 };
