4f3b937a6f4862e1e891b75527c0297e08b52861
Lars Poetzel <lars.poetzel@gmail.com>
2022-12-01 18:46:16 +0100

    filtering done

--- src/components/Listpage.js
+++ src/components/Listpage.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
 import Post from "./Post";
+const districts = [
+  "Mitte",
+  "Lichtenberg",
+  "Friedrichshain-Kreuzberg",
+  "Spandau",
+  "Charlottenburg-Wilmersdorf",
+  "Steglitz-Zehlendorf",
+  "Reinickendorf",
+  "Tempelhof-Schöneberg",
+  "Marzahn-Hellersdorf",
+  "Treptow-Köpenick",
+  "Neukölln",
+  "Pankow",
+];
 const ListPage = ({ posts }) => {
-  const results = posts.map((post, i) => <Post key={i} post={post} />);
-  return <main>{results}</main>;
+  const [district, setDistrict] = useState("");
+  const results = posts
+    .filter((post) => post.bezirk.includes(district))
+    .map((post, i) => <Post key={i} post={post} />);
+  return (
+    <main>
+      <div>
+        {districts.map((e) => {
+          return (
+            <button value={e} onClick={(e) => setDistrict(e.target.value)}>
+              {e}
+            </button>
+          );
+        })}
+      </div>
+      {results}
+    </main>
+  );
 };
 
 export default ListPage;
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,29 +2,44 @@ import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 const Post = ({ post }) => {
+  const dis = [post.bezirk];
+  const districts = [...new Set(dis)];
+  console.log(districts);
   const display = post.lat.length ? (
-    <div id="map">
-      <MapContainer
-        center={
-          post.lat.length
-            ? [post.lat.replaceAll(",", "."), post.lng.replaceAll(",", ".")]
-            : [52.5125277, 13.3871678]
-        }
-        zoom={12}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker
-          position={
+    <div>
+      <div id="map">
+        <MapContainer
+          center={
             post.lat.length
               ? [post.lat.replaceAll(",", "."), post.lng.replaceAll(",", ".")]
               : [52.5125277, 13.3871678]
           }
-        ></Marker>
-      </MapContainer>
+          zoom={12}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker
+            position={
+              post.lat.length
+                ? [post.lat.replaceAll(",", "."), post.lng.replaceAll(",", ".")]
+                : [52.5125277, 13.3871678]
+            }
+          ></Marker>
+        </MapContainer>
+      </div>
+      <div>
+        <a
+          id="maps-link"
+          href={`https://www.google.com/maps/search/?api=1&query=${post.strasse}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Show on the Map
+        </a>
+      </div>
     </div>
   ) : (
     <div>
