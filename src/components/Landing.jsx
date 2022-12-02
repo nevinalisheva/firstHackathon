28bac0ecee62c1f48f3ae182f85bf16e2259933b
Lars Poetzel <lars.poetzel@gmail.com>
2022-12-02 12:14:32 +0100

    wat

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,27 +1,26 @@
 import "./Landing.css";
 import React from "react";
-import Snowfall from 'react-snowfall';
+import Snowfall from "react-snowfall";
 
 function Landing() {
- 
-    return (
-      <div className="landing">
-        <Snowfall />
-        <div className="heading">
-            <h1>Christmas in <strong>Berlin</strong></h1>  
-        </div>
-        <div className="buttons">
-          <div className= "button">
-            <a href="/#list">Discover</a>
-          </div>
-          <div className="button">
-            <a href="/#a">Get Inspired</a>
-          </div>
-        </div>
-      
-        
+  return (
+    <div className="landing">
+      <Snowfall />
+      <div className="heading">
+        <h1>
+          Christmas in <strong>Berlin</strong>
+        </h1>
       </div>
-    );
-  }
-  
-  export default Landing;
+      <div className="buttons">
+        <div className="button">
+          <a href="/#list">Discover</a>
+        </div>
+        <div className="button">
+          <a href="/#a">Get Inspired</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
